999e111691e0876e32b8f762c94d9e1f22deacf5
designeng <denissavenok@gmail.com>
2016-03-04 14:18:36 +0300

    lifecycle.js reverted to es5

--- source/lib/lifecycle.js
+++ source/lib/lifecycle.js
@@ -5,108 +5,120 @@
  * http://www.opensource.org/licenses/mit-license.php
  */
 
-import when from 'when';
+(function(define){ 'use strict';
+define(function(require) {
 
-let safeNonFacetNames = {
-	id: { value: 1 }
-};
+	var when, safeNonFacetNames;
 
-function Lifecycle(plugins, pluginApi) {
-	this._plugins = plugins;
-	this._pluginApi = pluginApi;
-}
-
-Lifecycle.prototype = {
-	init: createLifecyclePhase(['create', 'configure', 'initialize']),
-	startup: createLifecyclePhase(['connect', 'ready']),
-	shutdown: createLifecyclePhase(['destroy'])
-};
-
-module.exports = Lifecycle;
-
-/**
- * Generate a method to process all steps in a lifecycle phase
- * @return {Function}
-*/
-function createLifecyclePhase(steps) {
-	steps = generateSteps(steps);
-
-	return function(proxy) {
-		let plugins, pluginApi;
-
-		plugins = this._plugins;
-		pluginApi = this._pluginApi.contextualize(proxy.id);
-
-		return when.reduce(steps, function (unused, step) {
-			return processFacets(step, proxy, pluginApi, plugins);
-		}, proxy);
+	when = require('when');
+	safeNonFacetNames = {
+		id: { value: 1 }
 	};
-}
 
-function processFacets(step, proxy, api, plugins) {
-	var promises, metadata, options, name, spec, facets, safeNames, unprocessed;
+	function Lifecycle(plugins, pluginApi) {
+		this._plugins = plugins;
+		this._pluginApi = pluginApi;
+	}
 
-	promises = [];
-	metadata = proxy.metadata;
-	spec = metadata.spec;
-	facets = plugins.facets;
-	safeNames = Object.create(plugins.factories, safeNonFacetNames);
-	unprocessed = [];
+	Lifecycle.prototype = {
+		init: createLifecyclePhase(['create', 'configure', 'initialize']),
+		startup: createLifecyclePhase(['connect', 'ready']),
+		shutdown: createLifecyclePhase(['destroy'])
+	};
 
-	for(name in spec) {
-		if(name in facets) {
-			options = spec[name];
-			if (options) {
-				processStep(promises, facets[name], step, proxy, options, api);
+	return Lifecycle;
+
+	/**
+	 * Generate a method to process all steps in a lifecycle phase
+	 * @return {Function}
+	 */
+	function createLifecyclePhase(steps) {
+		steps = generateSteps(steps);
+
+		return function(proxy) {
+			var plugins, pluginApi;
+
+			plugins = this._plugins;
+			pluginApi = this._pluginApi.contextualize(proxy.id);
+
+			return when.reduce(steps, function (unused, step) {
+				return processFacets(step, proxy, pluginApi, plugins);
+			}, proxy);
+		};
+	}
+
+	function processFacets(step, proxy, api, plugins) {
+		var promises, metadata, options, name, spec, facets, safeNames, unprocessed;
+
+		promises = [];
+		metadata = proxy.metadata;
+		spec = metadata.spec;
+		facets = plugins.facets;
+		safeNames = Object.create(plugins.factories, safeNonFacetNames);
+		unprocessed = [];
+
+		for(name in spec) {
+			if(name in facets) {
+				options = spec[name];
+				if (options) {
+					processStep(promises, facets[name], step, proxy, options, api);
+				}
+			} else if (!(name in safeNames)) {
+				unprocessed.push(name);
 			}
-		} else if (!(name in safeNames)) {
-			unprocessed.push(name);
+		}
+
+		if(unprocessed.length) {
+			return when.reject(unrecognizedFacets(proxy, unprocessed, spec));
+		} else {
+			return when.all(promises).then(function () {
+				return processListeners(step, proxy, api, plugins.listeners);
+			}).yield(proxy);
 		}
 	}
 
-	if(unprocessed.length) {
-		return when.reject(unrecognizedFacets(proxy, unprocessed, spec));
-	} else {
-		return when.all(promises).then(function () {
-			return processListeners(step, proxy, api, plugins.listeners);
-		}).yield(proxy);
-	}
-}
+	function processListeners(step, proxy, api, listeners) {
+		var listenerPromises = [];
 
-function processListeners(step, proxy, api, listeners) {
-	let listenerPromises = [];
+		for (var i = 0; i < listeners.length; i++) {
+			processStep(listenerPromises, listeners[i], step, proxy, {}, api);
+		}
 
-	for (let i = 0; i < listeners.length; i++) {
-		processStep(listenerPromises, listeners[i], step, proxy, {}, api);
+		return when.all(listenerPromises);
 	}
 
-	return when.all(listenerPromises);
-}
+	function processStep(promises, processor, step, proxy, options, api) {
+		var facet, pendingFacet;
 
-function processStep(promises, processor, step, proxy, options, api) {
-	let facet, pendingFacet;
+		if (processor && processor[step]) {
+			pendingFacet = when.defer();
+			promises.push(pendingFacet.promise);
 
-	if (processor && processor[step]) {
-		pendingFacet = when.defer();
-		promises.push(pendingFacet.promise);
-
-		facet = Object.create(proxy);
-		facet.options = options;
-		processor[step](pendingFacet.resolver, facet, api);
+			facet = Object.create(proxy);
+			facet.options = options;
+			processor[step](pendingFacet.resolver, facet, api);
+		}
 	}
-}
 
-function generateSteps(steps) {
-	return steps.reduce(reduceSteps, []);
-}
+	function generateSteps(steps) {
+		return steps.reduce(reduceSteps, []);
+	}
 
-function reduceSteps(lifecycle, step) {
-	lifecycle.push(step + ':before');
-	lifecycle.push(step);
-	lifecycle.push(step + ':after');
-	return lifecycle;
-}
+	function reduceSteps(lifecycle, step) {
+		lifecycle.push(step + ':before');
+		lifecycle.push(step);
+		lifecycle.push(step + ':after');
+		return lifecycle;
+	}
 
-function unrecognizedFacets(proxy, unprocessed, spec) {
-	return new Error('unrecognized facets in ' + proxy.id + ', maybe you forgot a plugin? ' + unprocessed.join(', ') + '\n' + JSON.stringify(spec));
-}
+	function unrecognizedFacets(proxy, unprocessed, spec) {
+		return new Error('unrecognized facets in ' + proxy.id + ', maybe you forgot a plugin? ' + unprocessed.join(', ') + '\n' + JSON.stringify(spec));
+	}
+
+});
+})(typeof define == 'function'
+	// AMD
+	? define
+	// CommonJS
+	: function(factory) { module.exports = factory(require); }
+);
